4d97a2384f958588f525da841b92780505aeff6f
Youva GAUDE <ygaude@student.42.fr>
2018-03-20 17:03:05 +0100

    BON GROS PUSH DEGEU

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
-path="../lem-in/maps/model_3"
-
-./../felix_lemin/lem-in < $path > fel.txt;
-./../lem-in/lem-in < $path > you.txt;
+echo "ant ="$3
+echo "#ants\n"$3 > $1/model_test
+tail -n +2 $1/$2 >> $1/model_test
+./../felix_lemin/lem-in < $1/model_test > fel.txt;
+./../lem-in/lem-in < $1/model_test > you.txt;
 ./corrector_lem_in_full fel.txt you.txt;
--- test_multi.sh
+++ test_multi.sh
@@ -1,19 +1,35 @@
-ant=0
+ant=1
 fin=100
 
-while test $ant != $fin
-  do
-    echo "ants = "$ant
-    echo "#ants\n"$ant > $1/model_test
-    tail -n 43 $1/model_3 >> $1/model_test
-    ./../felix_lemin/lem-in < $1/model_test > fel.txt
-    ./../lem-in/lem-in < $1/model_test > you.txt
-    ./corrector_lem_in fel.txt > fel1.txt
-    ./corrector_lem_in you.txt > you1.txt
-	if [[ $? -ne 0 ]]
-	then
-		exit
-	fi
-    diff fel1.txt you1.txt
-    ant=$(($ant + 1))
+rm -rf error
+mkdir error
+for i in `find $1 -type f`
+do
+	ant=1
+	echo "file =" $i
+	ferror=error/`basename $i`
+	mkdir $ferror
+	while test $ant != $fin
+	do
+		echo "#ants\n"$ant > model_test
+		tail -n +2 $i >> model_test
+		./../felix_lemin/lem-in < model_test > fel.txt
+		./../lem-in/lem-in < model_test > you.txt
+		./corrector_lem_in fel.txt > fel1.txt
+		./corrector_lem_in you.txt > you1.txt
+		diff fel1.txt you1.txt > /dev/null
+		if [[ $? -ne 0 ]]
+		then
+			if [[ `head -n 1 you1.txt` -gt `head -n 1 fel1.txt` ]]
+			then
+				state="worstest"
+			else
+				state="bestest"
+			fi
+			cat you.txt >> "$ferror/$ant($state)"
+			cat fel.txt >> "$ferror/$ant($state)"
+			diff fel1.txt you1.txt >> "$ferror/$ant($state)"
+		fi
+		ant=$(($ant + 1))
+	done
 done
